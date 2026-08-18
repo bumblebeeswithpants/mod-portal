870cf980bcd34097a95a0ba21ea3384847fff1d3
bumblebeeswithpants <bumblebeeswithpants@gmail.com>
2026-08-18 04:54:34 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-const PORTAL_CONFIG = {
+window.PORTAL_CONFIG = Object.freeze({
   API_URL: "https://script.google.com/macros/s/AKfycbwk-SOiTGWlS4A8yLN3PaR_H1GDwkuJIBfhp8fN114mA4Ysk0Soy-EnW-NPbCNZE_t1/exec"
-};
+});
